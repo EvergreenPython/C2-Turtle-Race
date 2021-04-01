942e5633003bd6fa8e8df7c254df4fc642688573
PythonEESD2021 <ctran@eesd.org>
2021-04-01 23:09:32 +0000

    functions

--- main.py
+++ main.py
@@ -1,68 +1,81 @@
 import turtle 
 import random
 
-cameraMan = turtle.Turtle() 
-cameraMan.shape("triangle")
-
-cameraMan.penup() 
-cameraMan.goto(-175, 140)
-cameraMan.speed(0)
-
-for i in range(16): 
-  cameraMan.write(i)
-  cameraMan.right(90)
-  cameraMan.forward(20)
-  cameraMan.pendown() 
-  cameraMan.forward(200)
-  cameraMan.left(180)
-  cameraMan.penup()
-  cameraMan.forward(220)
-  cameraMan.right(90)
-  cameraMan.forward(20)
-cameraMan.hideturtle() 
+# Functions: a group of related statements that perform a specific task 
+# functions help us breakdown our program into sections 
+# DRY: Don't Repeat Yourself 
+# Formula: 
+# def functionName (parameters): 
+#   BODY 
+#   optional 'return' statement 
 
 celine = turtle.Turtle() 
-celine.shape("turtle")
-celine.penup() 
-celine.goto(-200, 100)
-celine.color("gold") 
-
 trevor = turtle.Turtle() 
-trevor.shape("turtle")
-trevor.penup() 
-trevor.goto(-200, 60)
-trevor.color("red") 
-
-aaron = turtle.Turtle() 
-aaron.shape("turtle")
-aaron.penup() 
-aaron.goto(-200, 20)
-aaron.color("blue") 
-
+aaron = turtle.Turtle()
 emma = turtle.Turtle() 
-emma.shape("turtle")
-emma.penup() 
-emma.goto(-200, -20)
-emma.color("yellow") 
-
 arnav = turtle.Turtle() 
-arnav.shape("turtle")
-arnav.penup() 
-arnav.goto(-200, -60)
-arnav.color("silver") 
 
-# Create 4 more turtles, with different colors
-# Set their locations 
+def drawTrack(length): 
+  cameraMan = turtle.Turtle() 
+  cameraMan.shape("triangle")
 
-celine.pendown() 
-trevor.pendown()
-aaron.pendown() 
-emma.pendown() 
-arnav.pendown() 
+  cameraMan.penup() 
+  cameraMan.goto(-175, 140)
+  cameraMan.speed(0)
 
-for i in range(40): 
-  celine.forward(random.randint(1,15))
-  trevor.forward(random.randint(1,15))
-  aaron.forward(random.randint(1,15))
-  emma.forward(random.randint(1,15))
-  arnav.forward(random.randint(1,15))
+  for i in range(length): 
+    cameraMan.write(i)
+    cameraMan.right(90)
+    cameraMan.forward(20)
+    cameraMan.pendown() 
+    cameraMan.forward(200)
+    cameraMan.left(180)
+    cameraMan.penup()
+    cameraMan.forward(220)
+    cameraMan.right(90)
+    cameraMan.forward(20)
+  cameraMan.hideturtle() 
+
+def createTurtles(): 
+  celine.shape("turtle")
+  celine.penup() 
+  celine.goto(-200, 100)
+  celine.color("gold") 
+
+  trevor.shape("turtle")
+  trevor.penup() 
+  trevor.goto(-200, 60)
+  trevor.color("red") 
+ 
+  aaron.shape("turtle")
+  aaron.penup() 
+  aaron.goto(-200, 20)
+  aaron.color("blue") 
+
+  emma.shape("turtle")
+  emma.penup() 
+  emma.goto(-200, -20)
+  emma.color("yellow") 
+
+  arnav.shape("turtle")
+  arnav.penup() 
+  arnav.goto(-200, -60)
+  arnav.color("silver") 
+
+def startRace(): 
+  celine.pendown() 
+  trevor.pendown()
+  aaron.pendown() 
+  emma.pendown() 
+  arnav.pendown() 
+
+  for i in range(40): 
+    celine.forward(random.randint(1,15))
+    trevor.forward(random.randint(1,15))
+    aaron.forward(random.randint(1,15))
+    emma.forward(random.randint(1,15))
+    arnav.forward(random.randint(1,15))
+
+drawTrack(16) # function call 
+createTurtles()
+startRace() 
